8c81f6cd70df8c849e07feb3a5839be7f93e01b6
Marantesss <up201706917@fe.up.pt>
2019-11-04 12:50:31 +0000

    Made comments prettier

--- project-1/src/pieces.pl
+++ project-1/src/pieces.pl
@@ -1,10 +1,15 @@
 :-include('board_arrange.pl').
 
-% placement
+%---------------------------------------------------%
+%----------------- Piece Placement -----------------%
+%---------------------------------------------------%
+
 placePiece(Board, Piece, ColNum-RowLet, NewBoard):-
     isEmptyCellCoords(Board, ColNum-RowLet), % check if coords are allowed TODO -> check if placemente is allowed
     replaceCell(Board, Piece, ColNum-RowLet, RepBoard), % place piece
     rearrangeBoard(RepBoard, NewBoard). % rearrange board
 
-% movement
+%---------------------------------------------------%
+%----------------- Piece Movement ------------------%
+%---------------------------------------------------%
 
