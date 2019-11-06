d30cf62c49b2c70d97a27919c8967795c82235af
Simao Santos <simao.p.m.santos@gmail.com>
2019-11-06 14:48:41 +0000

    started new branch

--- project-1/src/rules.pl
+++ project-1/src/rules.pl
@@ -1,6 +1,10 @@
 % placement rules
 
+%verificar
+canPlace():-.
 
 % movement rules
 
-
+%se for rainha ou rei, verificar se tem jogadas possiveis
+%se nao: sendo rainha, eh consumida. sendo rei, game over
+canMove():-.
