96675c6cf3e84596d6fdbc62b58a722d35253ec4
Marantesss <up201706917@fe.up.pt>
2019-11-08 20:42:11 +0000

    Startegy planning for pieces

---
+++ project-1/src/bot.pl
@@ -0,0 +1,7 @@
+:- use_module(library(Random))
+
+/*
+1. Chooses a random piece (might not be on the board)
+2. Chooses a random option from the ->*possible*<- moves array given by the piece
+3. Make the play 
+*/
